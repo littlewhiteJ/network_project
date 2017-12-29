b43e816ed51e2f75eee19bec4ca29a3395959d26
littlewhiteJ <1500012873@pku.edu.cn>
2017-12-29 16:55:52 +0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -22,6 +22,7 @@ mail_password = raw_input('mail password: ')
 codedict = {}
 maildict = {}
 
+
 @app.route('/')
 def test():
     return 'server is okay'
@@ -37,6 +38,7 @@ class userInfoTable(userdb.Model):
 
 @app.route('/user',methods=['POST'])
 def check_user():
+    userdb.create_all()
     haveregisted = userInfoTable.query.filter_by(username=request.form['username']).all()
     if haveregisted.__len__() is not 0: 
         passwordRight = userInfoTable.query.filter_by(username=request.form['username'],password=request.form['password']).all()
@@ -51,7 +53,6 @@ def check_user():
 #register
 @app.route('/register',methods=['POST'])
 def register():
-    userdb.create_all()
     username = request.form['username']
     haveregisted = userInfoTable.query.filter_by(username=username).all()
     if haveregisted.__len__() is not 0: # judge if or not register
@@ -74,15 +75,23 @@ def register_():
         userInfo=userInfoTable(username=username,password=password)
         userdb.session.add(userInfo)
         userdb.session.commit()
-        userf = open('data/' + username + '.json', 'r+')
-        userdict = {}
-        userdict['username'] = username
-        userdict['email'] = maildict[username]
-        userdict['sum'] = 0
-        userdict['record_sum'] = 0
-        userdict['record'] = []
-        json.dump(userdict, userf)
-        userf.close()
+        with open('data/' + username + '.json', 'w') as userf:
+	        userdict = {}
+	        userdict['username'] = username
+	        userdict['email'] = maildict[username]
+	        userdict['sum'] = 0
+	        userdict['record_sum'] = 0
+            userdict['con_record'] = 0
+	        userdict['record'] = []
+	        json.dump(userdict, userf)
+        with open('data/all_ranking.json', 'r+') as arf:
+            ardict = json.load(arf)
+            ardict[username] = 0
+            json.dump(ardict, arf)
+        with open('data/con_ranking.json', 'r+') as crf:
+            crdict = json.load(crf)
+            crdict[username] = 0
+            json.dump(crdict, crf)
         return '0' # register okay
     return '1' # code error
 
@@ -112,11 +121,22 @@ def record():
     else: # if nobody records today
         haveregisted = userInfoTable.query.filter_by(username=username).all()
         for name in haveregisted:
-            f = open('data/' + name + '.json')
-            udict = json.load(f)
-            udict['sum'] += 1
-            json.dump(udict, f)
-            f.close()
+            with open('data/' + name + '.json') as f:
+                udict = json.load(f)
+                udict['sum'] += 1
+
+                todaytime = time.time()
+                yesttime = todaytime - 86400
+                ylocaltime = time.localtime(yesttime)
+                yesterday = str(ylocaltime.tm_year) + '_' + str(ylocaltime.tm_mon) + '_' + str(ylocaltime.tm_mday)
+                yfile = 'data/' + yesterday + '.json'
+                if os.path.isfile(yfile):
+                    with open(yfile) as yf:
+                        ydict = json.load(yf)
+                        ynamelst = ydict['namelst']
+                        if name not in ynamelst:
+                            udict['con_record'] = 0
+
         jsonf = open(filename, 'w')
         todaydict = {}
         namelst = []
@@ -138,11 +158,25 @@ def record():
     userf = open('data/' + username + '.json', 'r+')
     udict = json.load(userf)
     udict['record_sum'] += 1
+    udict['con_record'] += 1
+    con_record = udict['con_record']
     ulst = udict['record']
     ulst.append(str(time.time()))
     udict['record'] = ulst
     json.dump(udict, userf)
     userf.close()
+    
+    # modify all_ranking data
+    with open('data/all_ranking.json', 'r+') as arf:
+        ardict = json.load(arf)
+        ardict[username] += 1
+        json.dump(ardict, arf)
+
+    # modify con_ranking data
+    with open('data/con_ranking.json', 'r+') as crf:
+        crdict = json.load(crf)
+        crdict[username] = con_record
+        json.dump(crdict, crf)
 
     return '1' # record successfully
 
@@ -150,7 +184,7 @@ def record():
 def get_today_record():
     localtime = time.localtime(time.time())
     today = str(localtime.tm_year) + '_' + str(localtime.tm_mon) + '_' + str(localtime.tm_mday)
-    filename = '/data/' + today + '.json'
+    filename = 'data/' + today + '.json'
     if(os.path.exists(filename)):
         jsonf = open(filename, 'r+')
         todaydict = json.load(jsonf)
@@ -164,7 +198,7 @@ def get_today_record():
 def get_user_record():
     user = request.form['which_user']
     info = request.form['info']
-    filename = 'data' + user + '.json'
+    filename = 'data/' + user + '.json'
     if (os.path.exists(filename)):
         userf = open(filename, 'r')
         userdict = json.load(userf)
@@ -174,10 +208,54 @@ def get_user_record():
             return userdict['record_sum']
         if info == 2:
             return json.dumps(userdict['record'])
-        return '1'
+        if info == 3:
+            return userdict['con_record']
+        return '1' # info num is wrong
     else:
         return '0' # no that user
 
+@app.route('/all_ranking', methods = ['POST'])
+def all_ranking():
+    username = request.form['username']
+    with open('data/all_ranking.json', 'r') as arf:
+        ardict = json.load(arf)
+        lst = sorted(ardict.iteritems(), key = lambda d:d[1], reverse = True)
+        lstl = len(lst)
+        rank = 0
+        for i in range(lstl):
+            if lst[i][0] == username:
+                rank = i + 1
+        rankdict = {}
+        rankdict['rank'] = rank
+        rankdict['first2sixth'] = lst[:6]
+        return json.dumps(rankdict)
+
+@app.route('/con_ranking', methods = ['POST'])
+def con_ranking():
+    username = request.form['username']
+    with open('data/con_ranking.json', 'r') as crf:
+        crdict = json.load(crf)
+        lst = sorted(crdict.iteritems(), key = lambda d:d[1], reverse = True)
+        lstl = len(lst)
+        rank = 0
+        for i in range(lstl):
+            if lst[i][0] == username:
+                rank = i + 1
+        rankdict = {}
+        rankdict['rank'] = rank
+        rankdict['first2sixth'] = lst[:6]
+        return json.dumps(rankdict)
+
 if __name__ == '__main__':
+    if !os.path.isfile('data/all_ranking.json'):
+        with open('data/all_ranking.json', 'w') as arf:
+            ardict = {}
+            json.dump(ardict, arf)
+
+    if !os.path.isfile('data/con_ranking'):
+        with open('data/con_ranking.json', 'w') as crf:
+            crdict = {}
+            json.dump(crdict, crf)
+
     app.run(host = '0.0.0.0')
     
